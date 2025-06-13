c9eab7540af1e3504ee20ea54e37caa118162c99
Rasaq-ola <rasaqola196@gmail.com>
2025-06-14 00:11:17 +0100

    wrapping app with both Redux <provider> and custom CountProvider

--- alx-project-0x06/pages/_app.tsx
+++ alx-project-0x06/pages/_app.tsx
@@ -2,13 +2,17 @@ import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@/context/CountContext";
+import store from "@/store/store";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <CountProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CountProvider>
-  )
+    <Provider store={store}>
+      <CountProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CountProvider>
+    </Provider>
+  );
 }
